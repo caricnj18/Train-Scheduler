6dc590ad0f072552e7849d6beb7d1abf708a549b
carina cardona <carina.cardona89@gmail.com>
2018-03-31 09:39:47 -0400

    fully fuctional while still looking good

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,7 @@
+alert("test");
 /*Code this app to calculate when the next train will arrive; this should be relative to the current time.*/
 
 /* Users from many different machines must be able to view same train times.*/
-
-/*button*/
-$('#submitTrain').on('click', function () {
-    var $btn = $(this).button('loading')
-    // business logic...
-    $btn.button('reset')
-  })
-</sc>
 
 /*firebase*/
 
@@ -21,6 +14,144 @@ $('#submitTrain').on('click', function () {
     storageBucket: "trainschedule-da4dc.appspot.com",
     messagingSenderId: "7757671888"
   };
+
+
   firebase.initializeApp(config);
+
+var trainData = firebase.database();
+
+// 2. Populate Firebase Database with initial data (in this case, I did this via Firebase GUI)
+
+// 3. Button for adding trains
+$("#submitTrain").on("click", function(){
+
+	// Grabs user input
+	var trainName = $("#addName").val().trim();
+	var destination = $("#addStop").val().trim();
+	var firstTrainUnix = moment($("#addFirst").val().trim(), "HH:mm").subtract(10, "years").format("X");
+	var frequency = $("#addFreq").val().trim();
+
+	// Creates local "temporary" object for holding train data
+	var newTrain = {
+		name:  trainName,
+		destination: destination,
+		firstTrain: firstTrainUnix,
+		frequency: frequency
+	}
+
+	// Uploads train data to the database
+	trainData.ref().push(newTrain);
+
+	// Logs everything to console
+	console.log(newTrain.name);
+	console.log(newTrain.destination); 
+	console.log(firstTrainUnix);
+	console.log(newTrain.frequency)
+
+	// Alert
+	alert("Train successfully added");
+
+	// Clears all of the text-boxes
+	$("#trainNameInput").val("");
+	$("#destinationInput").val("");
+	$("#firstTrainInput").val("");
+	$("#frequencyInput").val("");
+
+	// Determine when the next train arrives.
+	return false;
+});
+
+
+// 4. Create Firebase event for adding trains to the database and a row in the html when a user adds an entry
+trainData.ref().on("child_added", function(childSnapshot, prevChildKey){
+
+	console.log(childSnapshot.val());
+
+	// Store everything into a variable.
+	var tName = childSnapshot.val().name;
+	var tDestination = childSnapshot.val().destination;
+	var tFrequency = childSnapshot.val().frequency;
+	var tFirstTrain = childSnapshot.val().firstTrain;
+
+	// Calculate the minutes until arrival using hardcore math
+	// To calculate the minutes till arrival, take the current time in unix subtract the FirstTrain time and find the modulus between the difference and the frequency  
+	var differenceTimes = moment().diff(moment.unix(tFirstTrain), "minutes");
+	var tRemainder = moment().diff(moment.unix(tFirstTrain), "minutes") % tFrequency ;
+	var tMinutes = tFrequency - tRemainder;
+
+	// To calculate the arrival time, add the tMinutes to the currrent time
+	var tArrival = moment().add(tMinutes, "m").format("hh:mm A"); 
+	console.log(tMinutes);
+	console.log(tArrival);
+
+	console.log(moment().format("hh:mm A"));
+	console.log(tArrival);
+	console.log(moment().format("X"));
+
+	// Add each train's data into the table 
+	$("#trainTable > tbody").append("<tr><td>" + tName + "</td><td>" + tDestination + "</td><td>" + tFrequency + "</td><td>" + tArrival + "</td><td>" + tMinutes + "</td></tr>");
+
+});
+
+
+// Assume the following situations. 
+
+// (TEST 1) 
+// First Train of the Day is 3:00 AM 
+// Assume Train comes every 3 minutes.
+// Assume the current time is 3:16 AM....
+// What time would the next train be...? (Use your brain first)
+// It would be 3:18 -- 2 minutes away
+
+// (TEST 2) 
+// First Train of the Day is 3:00 AM 
+// Assume Train comes every 7 minutes.
+// Assume the current time is 3:16 AM....
+// What time would the next train be...? (Use your brain first)
+// It would be 3:21 -- 5 minutes away
+
+
+// ==========================================================
+
+// Solved Mathematically
+// Test case 1: 
+// 16 - 00 = 16
+// 16 % 3 = 1 (Modulus is the remainder)
+// 3 - 1 = 2 minutes away
+// 2 + 3:16 = 3:18
+
+// Solved Mathematically  
+// Test case 2: 
+// 16 - 00 = 16
+// 16 % 7 = 2 (Modulus is the remainder)
+// 7 - 2 = 5 minutes away
+// 5 + 3:16 = 3:21
+
+
+
+
+
+
+
+/* event for when a train gets added
+garbs user input
+.ref().push to store in to firebase
+
+//button
+$('#submitTrain').on('click', function () {
+    var $btn = $(this).button('loading')
+    // business logic...
+    $btn.button('reset')
+  })
+
+child added
+store everything using var
+use moment to calculate next next arrival/minutesaway
+
+
+adding a row to the table */
+
+
+
 
 
